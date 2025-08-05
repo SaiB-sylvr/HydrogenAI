960167ab776738c9ff343e7c4b158109baa775ea
SaiB-sylvr <sai.b@sylvr.io>
2025-08-05 08:38:11 +0530

    Delete plugins directory

--- plugins/mongodb/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-MongoDB Plugin for MCP Server
-"""
-from .mongodb_tool import MongoDBPlugin
-
-# Export the plugin class for the plugin manager
-Plugin = MongoDBPlugin
-
-__all__ = ["MongoDBPlugin", "Plugin"]
--- plugins/rag/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-RAG Plugin for MCP Server
-"""
-from .rag_tool import RAGPlugin
-
-# Export the plugin class for the plugin manager
-Plugin = RAGPlugin
-
-__all__ = ["RAGPlugin", "Plugin"]
